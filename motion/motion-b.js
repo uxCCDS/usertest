c091de4c15a528d61068c6f6a9ce507d8558a791
Lin Wang <wl@wzpo.net>
2018-12-26 13:52:46 +0800

    fix

--- motion/motion-b.js
+++ motion/motion-b.js
@@ -3,8 +3,8 @@ $(function() {
   loaded = () => {
     $('.spinner-w').addClass('loaded');
     $('.spinner-c').hide();
-    $('.label1').fadeOut(100);
-    $('.label2').delay(100).fadeIn(300);
+    $('.label1').addClass('hidelabel1');
+    $('.label2').addClass('showlabel2');
 
     $('.loading-button').velocity(
       {
@@ -16,18 +16,33 @@ $(function() {
         easing: "easeOutQuad",
         progress: (elements, complete, remaining, start, tweenValue) => {
           if (complete == 1) {
-            
+            setTimeout(replay, 2000);
           }
         },
       }
     );
+
+    setTimeout(setClipPath, 320);
+
   }
   setClipPath = () => {
     $('#icon-spinner').attr('clip-path', 'url(#arrow-clip-path)');
     $('#arrow-clip-path').addClass('arrowup');
   }
 
+  replay = () => {
+    $('.spinner-w').removeClass('loaded');
+    $('.spinner-c').show();
+    $('.label1').removeClass('hidelabel1');
+    $('.label2').removeClass('showlabel2');
+    $('.loading-button').css('width', '76px');
+
+    $('#icon-spinner').attr('clip-path', 'url(#spinner-clip-path)');
+    $('#arrow-clip-path').removeClass('arrowup');
+
+    setTimeout(loaded, 2000);
+  }
+
   setTimeout(loaded, 2000);
-  setTimeout(setClipPath, 2650);
 
 });
